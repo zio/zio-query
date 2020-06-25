7deaacd94f72e76b73398fd48524b066d3d88790
Adam Fraser <adam.fraser@gmail.com>
2020-06-24 19:50:57 -0700

    establish baseline

--- zio-query/shared/src/test/scala/zio/query/ZIOBaseSpec.scala
+++ zio-query/shared/src/test/scala/zio/query/ZIOBaseSpec.scala
@@ -1,24 +1,11 @@
 package zio.query
 
-import zio.URIO
 import zio.duration._
 import zio.test._
-import zio.clock.Clock
 import zio.test.environment._
 
-trait ZIOBaseSpec extends RunnableSpec[TestEnvironment, Any] {
+trait ZIOBaseSpec extends DefaultRunnableSpec {
 
   override def aspects: List[TestAspect[Nothing, TestEnvironment, Nothing, Any]] =
     List(TestAspect.timeout(60.seconds))
-
-  override def runner: TestRunner[TestEnvironment, Any] =
-    defaultTestRunner
-
-  /**
-   * Returns an effect that executes a given spec, producing the results of the execution.
-   */
-  private[zio] override def runSpec(
-    spec: ZSpec[Environment, Failure]
-  ): URIO[TestLogger with Clock, ExecutedSpec[Failure]] =
-    runner.run(aspects.foldLeft(spec)(_ @@ _))
 }
