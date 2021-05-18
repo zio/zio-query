2893935251a55100d1e0b0490c9a9e1a9f727636
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-18 16:02:47 +0200

    Update silencer-lib, silencer-lib_2.13.5, ... to 1.7.4 (#198)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -186,11 +186,11 @@ object BuildHelper {
     scalacOptions := stdOptions ++ extraOptions(scalaVersion.value, optimize = !isSnapshot.value),
     libraryDependencies ++= {
       if (isDotty.value)
-        Seq("com.github.ghik" % "silencer-lib_2.13.5" % "1.7.3" % Provided)
+        Seq("com.github.ghik" % "silencer-lib_2.13.5" % "1.7.4" % Provided)
       else
         Seq(
-          "com.github.ghik" % "silencer-lib" % "1.7.3" % Provided cross CrossVersion.full,
-          compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.3" cross CrossVersion.full),
+          "com.github.ghik" % "silencer-lib" % "1.7.4" % Provided cross CrossVersion.full,
+          compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.4" cross CrossVersion.full),
           compilerPlugin(scalafixSemanticdb)
         )
     },
