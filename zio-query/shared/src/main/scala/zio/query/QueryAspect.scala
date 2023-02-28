f8b44c47ea0767fc211fae96d90dab7d3dea35a8
Adam Fraser <adam.fraser@gmail.com>
2023-02-28 10:02:05 -0800

    cleanup

--- zio-query/shared/src/main/scala/zio/query/QueryAspect.scala
+++ zio-query/shared/src/main/scala/zio/query/QueryAspect.scala
@@ -18,12 +18,23 @@ package zio.query
 
 import zio._
 
+/**
+ * A `QueryAspect` is an aspect that can be weaved into queries. You can think
+ * of an aspect as a polymorphic function, capable of transforming one data
+ * source into another, possibly enlarging the environment type.
+ */
 trait QueryAspect[+LowerR, -UpperR, +LowerE, -UpperE, +LowerA, -UpperA] { self =>
 
+  /**
+   * Applies the aspect to a query.
+   */
   def apply[R >: LowerR <: UpperR, E >: LowerE <: UpperE, A >: LowerA <: UpperA](query: ZQuery[R, E, A])(implicit
     trace: Trace
   ): ZQuery[R, E, A]
 
+  /**
+   * A symbolic alias for `andThen`.
+   */
   def >>>[
     LowerR1 >: LowerR,
     UpperR1 <: UpperR,
@@ -40,18 +51,6 @@ trait QueryAspect[+LowerR, -UpperR, +LowerE, -UpperE, +LowerA, -UpperA] { self =
    * Returns a new aspect that represents the sequential composition of this
    * aspect with the specified one.
    */
-  def @@[
-    LowerR1 >: LowerR,
-    UpperR1 <: UpperR,
-    LowerE1 >: LowerE,
-    UpperE1 <: UpperE,
-    LowerA1 >: LowerA,
-    UpperA1 <: UpperA
-  ](
-    that: QueryAspect[LowerR1, UpperR1, LowerE1, UpperE1, LowerA1, UpperA1]
-  ): QueryAspect[LowerR1, UpperR1, LowerE1, UpperE1, LowerA1, UpperA1] =
-    self >>> that
-
   def andThen[
     LowerR1 >: LowerR,
     UpperR1 <: UpperR,
@@ -73,7 +72,8 @@ trait QueryAspect[+LowerR, -UpperR, +LowerE, -UpperE, +LowerA, -UpperA] { self =
 object QueryAspect {
 
   /**
-   * Executes the specified workflows before and after the query.
+   * A query aspect that executes queries between two effects, `before` and
+   * `after`..
    */
   def around[R, A](
     before: ZIO[R, Nothing, A]
@@ -83,11 +83,19 @@ object QueryAspect {
         ZQuery.acquireReleaseWith[R1, E, A, B](before)(_ => after)(_ => query)
     }
 
+  /**
+   * A query aspect that executes requests between two effects, `before` and
+   * `after`, where the result of `before` can be used by `after`.
+   */
   def aroundDataSource[R, A](
     before: Described[ZIO[R, Nothing, A]]
   )(after: Described[A => ZIO[R, Nothing, Any]]): QueryAspect[Nothing, R, Nothing, Any, Nothing, Any] =
     fromDataSourceAspect(DataSourceAspect.around(before)(after))
 
+  /**
+   * A query aspect that executes requests with the specified data source
+   * aspect.
+   */
   def fromDataSourceAspect[R](
     dataSourceAspect: DataSourceAspect[R]
   ): QueryAspect[Nothing, R, Nothing, Any, Nothing, Any] =
@@ -96,6 +104,10 @@ object QueryAspect {
         query.mapDataSources(dataSourceAspect)
     }
 
+  /**
+   * A query aspect that limits data sources to executing at most `n` requests
+   * in parallel.
+   */
   def maxBatchSize(n: Int): QueryAspect[Nothing, Any, Nothing, Any, Nothing, Any] =
     fromDataSourceAspect(DataSourceAspect.maxBatchSize(n))
 }
