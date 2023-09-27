fb5eb535399b312603e5080afcaa9fe2e8e618a7
Adam Fraser <adam.fraser@gmail.com>
2023-09-27 16:30:43 -0700

    default test aspects

--- zio-query/shared/src/test/scala/zio/query/ZIOBaseSpec.scala
+++ zio-query/shared/src/test/scala/zio/query/ZIOBaseSpec.scala
@@ -4,5 +4,7 @@ import zio._
 import zio.test._
 
 trait ZIOBaseSpec extends ZIOSpecDefault {
-  override def aspects = Chunk(TestAspect.timeout(60.seconds))
+  override def aspects: Chunk[TestAspectPoly] =
+    if (TestPlatform.isJVM) Chunk(TestAspect.timeout(60.seconds), TestAspect.timed)
+    else Chunk(TestAspect.timeout(60.seconds), TestAspect.sequential, TestAspect.timed)
 }
