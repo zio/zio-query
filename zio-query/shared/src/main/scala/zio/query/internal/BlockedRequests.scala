f660fbc921856c9e2ac7312cb4bec9ae28500bd5
Kyri Petrou <kyri.petrou@outlook.com>
2024-04-02 13:19:56 +0800

    Fix linting

--- zio-query/shared/src/main/scala/zio/query/internal/BlockedRequests.scala
+++ zio-query/shared/src/main/scala/zio/query/internal/BlockedRequests.scala
@@ -16,10 +16,10 @@
 
 package zio.query.internal
 
-import zio.query.internal.BlockedRequests._
 import zio.query._
+import zio.query.internal.BlockedRequests._
 import zio.stacktracer.TracingImplicits.disableAutoTrace
-import zio.{Chunk, Exit, FiberId, Promise, Trace, UIO, Unsafe, ZEnvironment, ZIO}
+import zio.{Chunk, Exit, Promise, Trace, UIO, Unsafe, ZEnvironment, ZIO}
 
 import scala.annotation.tailrec
 import scala.collection.mutable
